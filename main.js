8c007c15e550490e996cf0d7b35c4ade9183fe31
Chyld Medford <chyld.medford@gmail.com>
2019-03-09 00:33:35 -0800

    add sleep

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ const port = process.env["PORT"];
 
 app.get("/", (req, res) => res.json({ hello: "world", port }));
 
+app.get("/add/:x/:y", (req, res) => {
+  const sleep = Math.random() * 3000;
+  const x = parseFloat(req.params.x);
+  const y = parseFloat(req.params.y);
+  setTimeout(() => {
+    res.json({ sum: x + y });
+  }, sleep);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
